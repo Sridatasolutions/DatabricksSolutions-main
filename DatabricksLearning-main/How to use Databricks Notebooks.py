cce501c96cffbd9327c84bb75968072be44d81a9
srini <srini@SRISRI>
2023-12-16 15:46:12 -0500

    Add project files.

---
+++ DatabricksLearning-main/How to use Databricks Notebooks.py
@@ -0,0 +1,66 @@
+# Databricks notebook source
+
+print('xyz')
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC # Header
+# MAGIC ## Sub header
+# MAGIC ### sub sub header
+
+# COMMAND ----------
+
+print("Hello World")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from samples.nyctaxi.trips;
+
+# COMMAND ----------
+
+print("Hello")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC println("Hello World")
+
+# COMMAND ----------
+
+#DBUTILS
+
+
+# COMMAND ----------
+
+dbutils.help()
+
+# COMMAND ----------
+
+dbutils.fs.help()
+
+# COMMAND ----------
+
+dbutils.fs.ls('/')
+
+# COMMAND ----------
+
+dbutils.fs.ls('dbfs:/databricks-datasets/')
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls '/'
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
